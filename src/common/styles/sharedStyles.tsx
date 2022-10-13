039460206e76d0d26bdbbfb38f94ea0e0ff84ecb
Thiago Ramos <thiagoramos@gmail.com>
2022-10-12 23:44:38 -0300

    feat: 🎸 W.I.P tailwindcss

--- src/common/styles/sharedStyles.tsx
+++ src/common/styles/sharedStyles.tsx
@@ -10,12 +10,9 @@ const Container = styled.div`
   min-height: 100vh;
 `
 const Main = styled.main`
-  padding: 5rem 0;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  flex
+  flex-row
+  order-1
 `
 
 const Title = styled.h1`
