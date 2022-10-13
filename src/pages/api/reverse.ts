41679a65ccb857f6155c6e63892f98949573641f
Thiago Ramos <thiagoramos@gmail.com>
2022-10-13 15:27:01 -0300

    fix: 🐛 Adjustment on reverse geocoordinates api

--- src/pages/api/reverse.ts
+++ src/pages/api/reverse.ts
@@ -9,23 +9,20 @@ export default async function handler(
 ) {
 
   const { query } = req;
+
   await fetch(
     `https://api.openweathermap.org/geo/1.0/reverse?lat=${query?.lat}&lon=${query?.lon}&limit=5&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
-  ).then(response => {
+  ).then(async response => {
     if (response.ok) {
-      return response.json();
+      const resp = await response.json()
+      return resp;
     }
-    console.error('Reverse API fail with');
   }).then(responseJson => {
+    if (!responseJson?.length) {
+      res.status(400).json(`Place with these coordinates not found.`);
+    }
     res.status(200).json(responseJson);
   }).catch(error => {
     throw new Error(`Reverse API fail with: ${error.message}`);
   });
-  // console.log(req, 'Reverse::enpoint', res)
-  // res.status(200).json([{
-  //   "name": "Coimbra",
-  //   "lat": 40.2111931,
-  //   "lon": -8.4294632,
-  //   "country": "PT"
-  // }])
 }
